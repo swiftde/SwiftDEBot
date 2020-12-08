b6d1f09fee18f4169c664dacc66ae193284f6839
Kilian Koeltzsch <me@kilian.io>
2020-12-08 21:44:43 +0100

    pull discord token from env instead of args

--- Sources/SwiftDEBot/main.swift
+++ Sources/SwiftDEBot/main.swift
@@ -1,7 +1,9 @@
 import Foundation
 import Sword
 
-let token = CommandLine.arguments[1]
+guard let token = ProcessInfo.processInfo.environment["DISCORD_TOKEN"] else {
+    fatalError("No token found in environment, please set DISCORD_TOKEN.")
+}
 let bot = Sword(token: token)
 
 bot.editStatus(to: "", playing: "swiftc -Ounchecked")
