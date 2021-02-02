123ee0bb80a8bfd7e9c89aa13301bba01c9abd4e
Kilian Koeltzsch <me@kilian.io>
2021-02-02 21:58:32 +0100

    Actually explain reason behind hearts message

--- Sources/SwiftDEBot/Command/Commands/MessageCommand+Hearts.swift
+++ Sources/SwiftDEBot/Command/Commands/MessageCommand+Hearts.swift
@@ -1,7 +1,9 @@
 extension MessageCommand {
     static func heartsMessage(to user: String) -> String {
         return """
-        Hey @\(user), das korrekte Herz Emoji sieht so aus ❤️. Alternativ sind auch 💜, 💙, 💚, 💛, 🧡, 🖤, 🤍, 🤎, 💖, 💝, 💞, 💗, 💘, 💕 und 💓 zulässig. Kann allen mal passieren, dass man da aus Versehen das Falsche wählt. Du solltest wissen, dass diese Herz Emoji und das von dir getippte unterschiedliche Bedeutungen haben. Die genannten drücken aus, dass du etwas magst, toll findest, liebst oder auf sonstige Art deine Zuneigung ausdrücken willst. Das von dir getippte allerdings zeigt, dass du ein Monster bist ✌️
+        Hey @\(user), es gibt eine ganze Reihe von Herz-Emoji: ❤️, 💜, 💙, 💚, 💛, 🧡, 🖤, 🤍, 🤎, 💖, 💝, 💞, 💗, 💘, 💕 und 💓.
+        Das von dir genutzte ♥️ ist allerdings Teil des Sets von Symbolen für Spielkarten, zusammen mit ♠️, ♣️ und ♦️ und ist daher
+        eher unpassend in anderen Kontexten.
         """
     }
 
