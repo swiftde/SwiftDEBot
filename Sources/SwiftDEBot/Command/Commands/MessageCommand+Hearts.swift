9e6ed902a20883ee3e61afa3bf60493cc6cdc851
Kilian Koeltzsch <me@kilian.io>
2021-02-02 22:08:32 +0100

    Remove newline in hearts message

--- Sources/SwiftDEBot/Command/Commands/MessageCommand+Hearts.swift
+++ Sources/SwiftDEBot/Command/Commands/MessageCommand+Hearts.swift
@@ -2,8 +2,7 @@ extension MessageCommand {
     static func heartsMessage(to user: String) -> String {
         return """
         Hey @\(user), es gibt eine ganze Reihe von Herz-Emoji: ❤️, 💜, 💙, 💚, 💛, 🧡, 🖤, 🤍, 🤎, 💖, 💝, 💞, 💗, 💘, 💕 und 💓.
-        Das von dir genutzte ♥️ ist allerdings Teil des Sets von Symbolen für Spielkarten, zusammen mit ♠️, ♣️ und ♦️ und ist daher
-        eher unpassend in anderen Kontexten.
+        Das von dir genutzte ♥️ ist allerdings Teil des Sets von Symbolen für Spielkarten, zusammen mit ♠️, ♣️ und ♦️ und ist daher eher unpassend in anderen Kontexten.
         """
     }
 
