a47c9dd6f2d770c25e85f90bbab5b32c613fbea1
Pascal Friedrich <pascal.friedrich@brickmakers.de>
2021-11-12 15:45:36 +0100

    Add a slap for everyone if a space is made before the punctuation mark

---
+++ Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
@@ -0,0 +1,14 @@
+import Sword
+
+extension Command where Trigger == Message {
+    static let spaceBeforePunctuationMark = Command(
+        run: { bot, message in
+            guard message.content.lowercased().contains(" ?") || message.content.contains(" !") else { return }
+            bot.addReaction(
+                "Ey \(message.user.mentionHandle), vor einem Satzzeichen gehört kein Leerzeichen. Wir sind hier nicht bei den Wilden! *slap*",
+                to: message.id,
+                in: message.channel.id
+            )
+        }
+    )
+}
