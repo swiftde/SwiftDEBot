d6b9ae440c8c1f478d24ea38cb11c7293e7b3bd4
Kilian Koeltzsch <me@kilian.io>
2021-11-12 16:06:55 +0100

    Use correct bot send func

--- Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
+++ Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
@@ -4,10 +4,9 @@ extension Command where Trigger == Message {
     static let spaceBeforePunctuationMark = Command(
         run: { bot, message in
             guard message.content.lowercased().contains(" ?") || message.content.contains(" !") else { return }
-            bot.addReaction(
+            bot.send(
                 "Ey \(message.author?.mentionHandle ?? "du Dulli"), vor einem Satzzeichen gehört kein Leerzeichen. Wir sind hier nicht bei den Wilden! *slap*",
-                to: message.id,
-                in: message.channel.id
+                to: message.channel.id
             )
         }
     )
