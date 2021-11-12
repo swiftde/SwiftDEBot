e9238a7a9919aa1daebc69b45cd15b888eaa8d23
Pascal Friedrich <pascal.friedrich@brickmakers.de>
2021-11-12 16:55:21 +0100

    Use regex for whitespace detection

--- Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
+++ Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
@@ -3,7 +3,7 @@ import Sword
 extension Command where Trigger == Message {
     static let spaceBeforePunctuationMark = Command(
         run: { bot, message in
-            guard message.content.lowercased().contains(" ?") || message.content.contains(" !") else { return }
+            guard message.content.range(of: "\\s[!?.]+$", options: .regularExpression) != nil else { return }
             bot.send(
                 "Ey \(message.author?.mentionHandle ?? "du Dulli"), vor einem Satzzeichen gehört kein Leerzeichen. Wir sind hier nicht bei den Wilden! *slap*",
                 to: message.channel.id
