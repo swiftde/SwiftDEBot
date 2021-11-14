f52da930c8c83fbd692de685c4a57ef85f25cf50
Kilian Koeltzsch <me@kilian.io>
2021-11-14 20:15:43 +0100

    use cows package instead of straight text source

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "SwiftDEBot",
     dependencies: [
         .package(url: "https://github.com/Azoy/Sword.git", from: "0.9.2"),
+        .package(url: "https://github.com/AlwaysRightInstitute/cows.git", from: "1.0.0"),
 
         // Remove me when upgrading to a more current version of Swift.
         .package(url: "https://github.com/antitypical/Result", from: "5.0.0"),
@@ -15,6 +16,7 @@ let package = Package(
             name: "SwiftDEBot",
             dependencies: [
                 "Sword",
+                "cows",
                 "Result"
             ]),
     ]
