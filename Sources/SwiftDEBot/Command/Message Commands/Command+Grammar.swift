c36ae3b0207a0e46d80012049dfa6a4c1a29d826
Kilian Koeltzsch <me@kilian.io>
2021-11-15 12:37:33 +0100

    user user IDs for grammar checking

--- Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
+++ Sources/SwiftDEBot/Command/Message Commands/Command+Grammar.swift
@@ -1,17 +1,21 @@
 import Sword
 
 extension Command where Trigger == Message {
-    fileprivate static let grammarOffenders = ["Veit", "Fabian"]
+    fileprivate static let grammarOffenders = [
+        "137444620631015424", // Veit
+        "296369044775501824" // Fabian
+    ]
 
     static let spaceBeforePunctuationMark = Command(
         run: { bot, message in
             guard
-                let username = message.author?.username,
-                grammarOffenders.contains(username),
+                let userID = message.author?.id,
+                grammarOffenders.contains(String(describing: userID)),
                 message.content.range(of: "\\s[!?.]+$", options: .regularExpression) != nil
             else {
                 return
             }
+
             bot.send(
                 "Ey \(message.author?.mentionHandle ?? "du Dulli"), vor ein Satzzeichen gehört kein Leerzeichen. Wir sind hier nicht bei den Wilden! *slap*",
                 to: message.channel.id
