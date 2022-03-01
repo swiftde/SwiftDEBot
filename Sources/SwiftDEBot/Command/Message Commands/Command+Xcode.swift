8b8a1de80487610488f1b9cab69be91c8ca01346
Kilian Koeltzsch <me@kilian.io>
2022-03-01 13:16:21 +0100

    Fix XCode/xCode matching

--- Sources/SwiftDEBot/Command/Message Commands/Command+Xcode.swift
+++ Sources/SwiftDEBot/Command/Message Commands/Command+Xcode.swift
@@ -8,7 +8,7 @@ extension Command where Trigger == Message {
     static let xcodeTypo = Command(
         run: { bot, message in
             let content = message.content
-            guard content.contains("XCode") || content.contains("xCode"),
+            guard content.contains(" XCode") || content.contains(" xCode"),
                   let handle = message.author?.mentionHandle else {
                 return
             }
