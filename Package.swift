efd7a71f9e30d2ce4266cafb037982f17ff9f075
Kilian Koeltzsch <me@kilian.io>
2023-04-14 23:46:53 +0200

    Use tagged version of DiscordBM

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v12),
     ],
     dependencies: [
-        .package(url: "https://github.com/MahdiBM/DiscordBM", revision: "53e8bd10a4489cd5a8c2c7a47d8801e20a3df530"),
+        .package(url: "https://github.com/MahdiBM/DiscordBM", exact: "1.0.0-beta.49"),
         .package(url: "https://github.com/swift-server/async-http-client", from: "1.15.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
         .package(url: "https://github.com/AlwaysRightInstitute/cows.git", from: "1.0.0"),
