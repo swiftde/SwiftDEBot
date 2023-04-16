1377154fbede23f6c0679a85fe437294266b3cad
Kilian Koeltzsch <me@kilian.io>
2023-04-16 11:06:59 +0200

    remove host uptime

--- Sources/SwiftDEBot/Command/Message Commands/Uptime.swift
+++ Sources/SwiftDEBot/Command/Message Commands/Uptime.swift
@@ -6,8 +6,7 @@ struct UptimeCommand: MessageCommand {
 
     func run(client: DiscordClient, message: Gateway.MessageCreate) async throws {
         guard message.content == "!uptime" else { return }
-        let hostUptime = shell.uptime().stdout
         let containerUptime = shell.ps("-o etime= -p 1").stdout.trimmingCharacters(in: .whitespacesAndNewlines)
-        try await client.send("Uptime: \(containerUptime)\nHost: \(hostUptime)", to: message.channel_id)
+        try await client.send("Uptime: \(containerUptime)", to: message.channel_id)
     }
 }
