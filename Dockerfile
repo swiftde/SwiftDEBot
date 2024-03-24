a495d05e96c1c616dbbdcaa42a933a6cd5c86b8c
Kilian Koeltzsch <me@kilian.io>
2024-03-24 22:14:15 +0100

    bump builds to use swift:5.9

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM swift:5.7 AS build
+FROM swift:5.9 AS build
 
 WORKDIR /app
 COPY . ./
 RUN swift build --configuration release
 
-FROM swift:5.7-slim
+FROM swift:5.9-slim
 WORKDIR /app
 COPY --from=build /app/.build/release/SwiftDEBot .
 
--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftDEBot",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
     ],
     dependencies: [
         .package(url: "https://github.com/MahdiBM/DiscordBM", exact: "1.0.0-beta.49"),
